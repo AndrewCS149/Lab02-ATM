2978d424d8372b64125a50de2ad80dbeaaf78018
Andrew Smith <andrewcs149@gmail.com>
2020-07-07 14:58:46 -0700

    Add Deposit().

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace ATM
 {
@@ -11,26 +12,39 @@ namespace ATM
 
             Console.WriteLine(ViewBalance());
             Console.WriteLine(WithDraw(500));
+            Console.WriteLine(Deposit(1000));
 
         }
 
         /// <summary>
-        /// Displays the current balance to the user.
+        /// Displays the current balance to the user
         /// </summary>
-        /// <returns>The current balance.</returns>
+        /// <returns>The current balance</returns>
         public static decimal ViewBalance()
         {
             return Balance;
         }
 
         /// <summary>
-        /// Subtracts from the Balance.
+        /// Subtracts from the current balance
         /// </summary>
         /// <param name="amount">Amount to withdraw.</param>
-        /// <returns>The new Balance.</returns>
+        /// <returns>The new balance</returns>
         public static decimal WithDraw(decimal amount)
         {
-            return Balance - amount;
+            Balance -= amount;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Adds to the current balance
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        /// <returns>The new balance</returns>
+        public static decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
         }
     }
 }
