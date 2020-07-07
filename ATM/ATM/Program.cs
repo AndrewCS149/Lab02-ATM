4a0e9cf3443633047755518dc8085a5b8bdf2276
Andrew Smith <andrewcs149@gmail.com>
2020-07-07 14:56:04 -0700

    Create WithDraw() and ViewBalance()

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -4,9 +4,33 @@ namespace ATM
 {
     class Program
     {
+        static public decimal Balance = 1000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(ViewBalance());
+            Console.WriteLine(WithDraw(500));
+
+        }
+
+        /// <summary>
+        /// Displays the current balance to the user.
+        /// </summary>
+        /// <returns>The current balance.</returns>
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        /// <summary>
+        /// Subtracts from the Balance.
+        /// </summary>
+        /// <param name="amount">Amount to withdraw.</param>
+        /// <returns>The new Balance.</returns>
+        public static decimal WithDraw(decimal amount)
+        {
+            return Balance - amount;
         }
     }
 }
